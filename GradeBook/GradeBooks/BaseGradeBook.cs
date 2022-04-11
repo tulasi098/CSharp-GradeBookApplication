661b7383006476e32c5b66c6af264a5f9524f8c9
tulasi098 <tulasisomala16@gmail.com>
2022-04-11 19:31:49 +0530

    Added GradeBookTypes

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -20,6 +20,7 @@ namespace GradeBook.GradeBooks
             Students = new List<Student>();
         }
 
+        public GradeBookType Type { get; set; }
         public void AddStudent(Student student)
         {
             if (string.IsNullOrEmpty(student.Name))
@@ -265,6 +266,20 @@ namespace GradeBook.GradeBooks
                              select type).FirstOrDefault();
             
             return JsonConvert.DeserializeObject(json, gradebook);
+        }
+    }
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked; 
         }
     }
 }
